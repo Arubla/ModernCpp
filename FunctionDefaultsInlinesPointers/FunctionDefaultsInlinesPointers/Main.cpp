bb7307aad3ba28ac5f5a52bcdfe563819bc8799b
arubla <arunblatchley@gmail.com>
2023-01-12 15:05:06 -0500

    whoops

---
+++ FunctionDefaultsInlinesPointers/FunctionDefaultsInlinesPointers/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void CreateWindow(const char* title/*the onle parameter required*/, int x = -1, int y = -1, int width = -1, int height = -1) {
+	return;
+}
+
+int main() {
+
+	return 0;
+}
