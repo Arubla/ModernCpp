dc34d19072b0927ca1c38a19d1293791bb5a77e1
arubla <arunblatchley@gmail.com>
2023-01-12 16:21:15 -0500

    function pointers

--- FunctionDefaultsInlinesPointers/FunctionDefaultsInlinesPointers/Main.cpp
+++ FunctionDefaultsInlinesPointers/FunctionDefaultsInlinesPointers/Main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 #include "Header.h"
+
+void Print(int count, char ch) {
+	for (int i = 0; i < count; i++) {
+		std::cout << ch;
+	}
+	std::cout << std::endl;
+}
+
+void EndMessage() {
+	std::cout << "Goodbye";
+}
+
 inline int Square(int x) {
 	return x * x;
 }//this replaces the call with the function itself at compilation, saving memory
@@ -9,5 +21,13 @@ int main() {
 	using namespace std;
 	int val{ 5 };
 	cout << Square(val + 1) << endl;
+
+	Print(5, '#');
+	void(*pfn) (int, char) = &Print;//function pointer init
+	(*pfn)(8, '@');//examples of calling function pointers
+	pfn(5, '+');
+
+	atexit(EndMessage);//internal pointer
+
 	return 0;
 }
